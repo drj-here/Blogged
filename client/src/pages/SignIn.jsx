bcf3c502eccbb16197933b4383d9e478c689d16e
Dishu <dishuraj129@gmail.com>
2024-04-27 23:52:14 +0530

    Added redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,39 +1,39 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
+import { signInFailure, signInStart,signInSuccess } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData,setFormData]=useState({})
-  const [errorMessage,setErrorMessage]=useState(null)
-  const [loading,setLoading]=useState(false)
+  const {loading,error:errorMessage}=useSelector((state)=>state.user)
   const navigate=useNavigate()
+  const dispatch=useDispatch()
   const handleChange=(e)=>{
     setFormData({...formData,[e.target.id]:e.target.value.trim()})
   }
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+      return dispatch(signInFailure('Please fill out all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
-      const res = await fetch('/api/auth/signin', {
+      dispatch(signInStart())
+      const res = await fetch('/api/auth/sign-in', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+      
       if (res.ok) {
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message))
     }
   };
   return (
@@ -43,9 +43,9 @@ export default function SignIn() {
       <div className='flex-1'>
         <Link to='/' className='font-bold dark:text-white text-4xl'>
           <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-            Sahand's
+            Blogged 
           </span>
-          Blog
+         
         </Link>
         <p className='text-sm mt-5'>
           This is a demo project. You can sign in with your email and password
@@ -57,7 +57,7 @@ export default function SignIn() {
       <div className='flex-1'>
         <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
           <div>
-            <Label value='Your email' />
+            <Label value='Your email'/>
             <TextInput
               type='email'
               placeholder='name@company.com'
