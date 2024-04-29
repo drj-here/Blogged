f95df6285d84f1bcc00245f4ce2a85d01b144e75
Dishu <dishuraj129@gmail.com>
2024-04-29 11:58:54 +0530

    Added dark mode

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import { Avatar, Button, Dropdown, DropdownDivider, DropdownHeader, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
-import { useSelector } from 'react-redux';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function Header() {
+  const dispatch=useDispatch()
     const path = useLocation().pathname;
+    const {theme}=useSelector((state)=>state.theme)
     const {currentUser}=useSelector((state)=>state.user)
   return (
     <Navbar className='border-b-2'>
@@ -30,8 +32,8 @@ export default function Header() {
         <AiOutlineSearch />
       </Button>
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
-          <FaMoon />
+        <Button className='w-12 h-10 hidden sm:inline' color='gray' pill onClick={()=>dispatch(toggleTheme())}>
+          {theme==='light'?<FaSun/>:<FaMoon />}
         </Button>
         {currentUser?(
           <Dropdown
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,9 +3,10 @@ import userReducer from './user/userSlice'
 import storage from "redux-persist/lib/storage";
 import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
-
+import themeReducer from './theme/themeSlice'
 const rootReducer=combineReducers({
     user:userReducer, 
+    theme:themeReducer,
 })
 
 const persistConfig={
