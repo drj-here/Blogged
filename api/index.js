9948cf3aae1056a59126e45605bdfa010785f885
Dishu <dishuraj129@gmail.com>
2024-05-01 12:42:41 +0530

    Added create api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import mongoose from 'mongoose';
 import userRoute from './routes/user.route.js'
 import authRoute from './routes/auth.route.js'
+import postRoute from './routes/post.route.js'
 import cookieParser from 'cookie-parser';
 dotenv.config();
 
@@ -23,7 +24,7 @@ app.listen(process.env.PORT,()=>{
 
 app.use('/api/user',userRoute)
 app.use('/api/auth',authRoute)
-
+app.use('/api/post',postRoute) 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
     const message=err.message || 'Internal server error'
---
+++ api/routes/post.route.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import { create } from '../controllers/post.controller'
+import { verifyToken } from '../utils/verifyUser';
+
+const router=express.Router()
+
+router.post('/create',verifyToken,create)
+export default router;
