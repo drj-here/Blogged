475cc1e218d55ea75f3289520a0af778bcacaac1
Dishu <dishuraj129@gmail.com>
2024-05-03 13:02:12 +0530

    users functionalities for the admin

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -48,7 +48,7 @@ export const updateUser=async(req,res,next)=>{
 }
 
 export const deleteUser=async(req,res)=>{
-    if(req.user.id!==req.params.userId){
+    if(!req.user.isAdmin && req.user.id!==req.params.userId){
         return next(errorHandler(403,'You are not allowed to delete this user'))
     }
     try {
@@ -110,3 +110,4 @@ export const getUsers=async(req,res,next)=>{
         next(error)
     }
 }
+
