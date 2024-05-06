73fd7e0d9fbf84c0556dbcef930b06205dc2211e
Dishu <dishuraj129@gmail.com>
2024-05-06 11:27:53 +0530

    added pre installation

--- api/index.js
+++ api/index.js
@@ -6,8 +6,10 @@ import authRoute from './routes/auth.route.js'
 import postRoute from './routes/post.route.js'
 import commentRoute from './routes/comment.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 dotenv.config();
 
+const __dirname=path.resolve();
 const app=express()
 app.use(express.json())
 app.use(cookieParser());
@@ -27,6 +29,12 @@ app.use('/api/user',userRoute)
 app.use('/api/auth',authRoute)
 app.use('/api/post',postRoute) 
 app.use('/api/comment',commentRoute)
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode || 500;
     const message=err.message || 'Internal server error'
